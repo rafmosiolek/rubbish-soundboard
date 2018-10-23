c8a67e4fbcddd4367caa968ab62559fb6d99ce03
RafM <rkjmosiolek@gmail.com>
2018-10-23 14:35:21 +0100

    create button class

---
+++ Button.js
@@ -0,0 +1,7 @@
+class Button {
+    constructor(label, x, y) {
+        this.label = label;
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,9 @@
+var dogButton = new Button('Dog', 10, 10);
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(220);
+}
