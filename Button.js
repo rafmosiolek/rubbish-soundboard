a8430de6b4d06fe74b64daca1bbc81baa0cac47d
RafM <rkjmosiolek@gmail.com>
2018-10-24 09:48:57 +0100

    add draw method to the button class

--- Button.js
+++ Button.js
@@ -4,4 +4,10 @@ class Button {
         this.x = x;
         this.y = y;
     }
+
+    draw() {
+        rect(this.x, this.y, 100, 30);
+        text(this.label, this.x + 50, this.y + 20);
+        textAlign(CENTER);
+    }
 }
--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 var dogButton = new Button('Dog', 10, 10);
 
+
 function setup() {
     createCanvas(400, 400);
 }
 
 function draw() {
     background(220);
+    dogButton.draw();
 }
